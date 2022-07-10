3f9bcc45023a3ca780eb0fc63fcb5ebcb870eb24
ashishkant <ashishkumarkant@gmail.com>
2022-07-10 15:24:12 +0530

    fourth commit

---
+++ loopadding.py
@@ -0,0 +1,9 @@
+ch = "y"
+
+while(ch=="y" or ch=="Y"):
+    a = int(input("enter the first number : "))
+    b = int(input("enter the second number : "))
+    print("SUm : ",(a+b))
+    ch=input("wana more [y/n]: ")
+
+print("Thank you.")
