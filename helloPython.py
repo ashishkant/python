e2ef041b189200db68fd99edd91114cbf030d72d
ashishkant <ashishkumarkant@gmail.com>
2022-07-30 21:50:44 +0530

    tenth

---
+++ helloPython.py
@@ -0,0 +1,2 @@
+print("hello Python I ma going to create a demo video over you")
+print("hope it is a fun inbetween you and I")
--- importantmsg.py
+++
@@ -1,1 +0,0 @@
-print("Hello programmer i create this python code using a basic text editor")
